4ef5ec2e8e8f7b94352c8dfa8b5e02381caa39d0
your-name <your-email>
2026-02-12 01:42:19 +0530

    Added average score from object array problem

---
+++ javascript/averageOfarray.js
@@ -0,0 +1,19 @@
+ const arr = [
+ { name: "A", score: 80 },
+ { name: "B", score: 90 },
+ { name: "C", score: 70 }
+]
+
+
+
+let sum = 0 
+
+const avg = 0 
+
+arr.forEach((item)=>{
+  sum += item.score 
+  
+}
+)
+
+console.log(sum/arr.length)
