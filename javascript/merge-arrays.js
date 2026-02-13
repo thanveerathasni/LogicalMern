c8a2e36ee6c749911889d2d31bbe25bdbd2781da
your-name <your-email>
2026-02-13 13:38:17 +0530

    JS Logic: Merge arrays alternatively without built-ins

--- javascript/merge-arrays.js
+++ javascript/merge-arrays.js
@@ -1,17 +1,44 @@
 
 
-let a =[1,3,5], b =
-[2,4,6],res =[]
+// let a =[1,3,5], b =
+// [2,4,6],res =[]
 
-for(let i =0 ;i< a.length;i++){
-  res.push(a[i])
+// for(let i =0 ;i< a.length;i++){
+//   res.push(a[i])
+  
+// }
+// for(let i =0 ;i <b.length ;i++){
+//   res.push(b[i])
+  
+// }
+
+
+// console.log(res)
+
+
+
+
+
+
+let a = [1,2,3];
+let b = ['a','b'];
+
+let res = []
+
+let max = Math.max(a.length,b.length)
+
+for(let i = 0 ; i < max ; i++){
+  
+  if(i<a.length) return res.push(a[i])
+  if(i<b.length) return res.push(b[i])
   
 }
-for(let i =0 ;i <b.length ;i++){
-  res.push(b[i])
-  
-}
+
 
 
 console.log(res)
+
+
+
+
 
