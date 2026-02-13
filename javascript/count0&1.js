344ff5c808a69ab71ab5c3dc1369aaf22c34dc9d
your-name <your-email>
2026-02-13 09:24:21 +0530

    JS Logic: Count zeros and ones without if or built-ins

---
+++ javascript/count0&1.js
@@ -0,0 +1,45 @@
+// Count 0s and 1s (no built-ins, no if)
+
+
+
+
+let array = [1,1,1,0,1,0,1,0,1];
+
+let sum = 0
+
+for(let i = 0 ;i<array.length;i++){
+  sum+= array[i]
+  
+}
+
+let one = sum 
+let zero =array.length -  one
+
+console.log(one)
+console.log(zero)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
